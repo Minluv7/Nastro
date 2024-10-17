576b977deca0e791968ef227317073bb365012b5
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-10-17 20:29:46 +0200

    change styling css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,20 +54,18 @@ export default function Home() {
     };
 
     return (
-        <div className='flex flex-wrap items-center h-screen justify-center'
-         >
-           
+        <div className="flex flex-wrap items-center min-h-screen ">
             <DotLottieReact
-                className='w-[50%] items-center'
+                className="w-full md:w-[50%] items-center"
                 src="https://lottie.host/25e4cfe2-7935-4ff5-b534-8e4106ba61a2/TWME2CFNAO.json"
                 loop
                 autoplay
             />
-            <div className='flex justify-center'>
+            <div className="w-full md:w-auto flex flex-wrap justify-center">
                 <form 
-                    className="bg-white p-12 w-[90%] md:w-[35rem] flex flex-col items-center rounded-[1rem] md:rounded-[1rem_10rem] transition-all duration-300"
+                    className="bg-white p-6 md:p-14 w-[90%] md:w-[35rem] flex flex-col items-center rounded-[1rem] md:rounded-[1rem_10rem] transition-all duration-300"
                     onSubmit={handleSubmit}
-                    >
+                >
                     <label className="flex flex-col w-full text-gray-700 space-y-4">
                         <div className="flex flex-col">
                             <span>Your name:</span>
@@ -81,7 +79,7 @@ export default function Home() {
                         </div>
                         
                         <div className="flex flex-col">
-                            <span >Birthplace:</span>
+                            <span>Birthplace:</span>
                             <input 
                                 className="mt-2 p-3 rounded-md border border-gray-300 focus:border-blue-400 focus:ring focus:ring-blue-200 outline-none transition-all duration-300"
                                 type="text" 
@@ -90,22 +88,20 @@ export default function Home() {
                                 required
                             />
                         </div>
-                        
-                        <div className="flex flex-col w-[40%]">
-                            <span className=" mb-2 ">Birthday</span>
-                            <input 
-                                className="mt-2 p-3 border-b-2 border-gray-400 bg-transparent text-center text-xl focus:outline-none focus:border-blue-400 transition-all duration-300 placeholder-gray-500"
-                                type="date" 
-                                name="birthday"
-                                placeholder='DD/MM/YYYY'
-                                required
-                            
-                            />
-                        </div>
-
-                        <div className='flex gap-4'>
-                            <div className="flex flex-col w-[40%]">
-                                <span className=" mb-2 ">Time of birth</span>
+                       
+                            <div className="flex flex-col w-[100%] md:w-[40%]">
+                                <span className="mb-2">Birthday</span>
+                                <input 
+                                    className="mt-2 p-3 border-b-2 border-gray-400 bg-transparent text-center text-xl focus:outline-none focus:border-blue-400 transition-all duration-300 placeholder-gray-500"
+                                    type="date" 
+                                    name="birthday"
+                                    placeholder='DD/MM/YYYY'
+                                    required
+                                />
+                            </div>
+                            <div className='flex justify-between gap-4'>
+                            <div className="flex flex-col w-[100%] md:w-[40%]">
+                                <span className="mb-2">Time of birth</span>
                                 <input 
                                     className="mt-2 p-3 border-t-2 border-b-2 border-gray-400 bg-transparent text-center text-xl focus:outline-none focus:border-blue-400 transition-all duration-300"
                                     type="time" 
@@ -114,21 +110,20 @@ export default function Home() {
                                     placeholder='HH:MM'
                                 />
                             </div>
-                                <button 
-                                    className="mt-8 w-[10rem] ml-auto bg-gradient-to-r tracking-widest hover:from-black hover:to-black bg-background text-textColor uppercase py-3 rounded-md shadow-lg transition-all duration-300 transform hover:scale-105"
-                                    type="submit"
-                                >
-                                    Calculate
-                                </button>
+                        
+                        <button 
+                            className="mt-8 w-full md:w-[10rem] bg-gradient-to-r tracking-widest hover:from-black hover:to-black bg-background text-textColor uppercase py-3 rounded-md shadow-lg transition-all duration-300 transform hover:scale-105"
+                            type="submit"
+                        >
+                            Calculate
+                        </button>
                         </div>
                     </label>
                 </form>
-
-
+    
                 <Popup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)}>
                     <h2>Horoscope Results:</h2>
                     <p>Full name: {fullName}</p>
-
                     {/* Container for planet and house results */}
                     <div className="flex flex-col md:flex-row gap-4 mt-4">
                         <PlanetResults formData={formData} />
@@ -138,4 +133,5 @@ export default function Home() {
             </div>
         </div>
     );
+    
 }
--- src/components/popup.tsx
+++ src/components/popup.tsx
@@ -1,13 +1,12 @@
-// components/Popup.tsx
 const Popup = ({ isOpen, onClose, children }: { isOpen: boolean, onClose: () => void, children: React.ReactNode }) => {
     if (!isOpen) return null;
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded shadow-lg w-[80%] max-w-[500px] relative">
+            <div className="bg-white p-6 rounded shadow-lg w-[90%] max-w-[500px] max-h-[80vh] overflow-y-auto relative">
                 {/* X knop om de popup te sluiten */}
                 <button 
-                    className="absolute top-2 right-2 text-xl text-gray-600 hover:text-red-600" 
+                    className="absolute top-2 right-2 text-[2rem] text-gray-600 hover:text-red-600" 
                     onClick={onClose}
                     aria-label="Close popup"
                 >
